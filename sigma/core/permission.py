f5a45bc3c4a0a51d1e7b8f3f6ecb9192e93d58c7
Patrick Auernig <patrick.auernig@gmail.com>
2017-02-15 06:23:45 +0100

    move blacklist check to permission.py

--- sigma/core/permission.py
+++ sigma/core/permission.py
@@ -157,3 +157,24 @@ def check_permitted(self, user, channel, server):
         return False, embed_content
 
     return True, None
+
+
+def check_black(db, message):
+    if message.author.id in permitted_id:
+        return False
+
+    if not message.server:
+        return False
+
+    black_channel   = False
+    black_user      = False
+    server_is_black = False
+
+    channel_blacklist = db.get_settings(message.server.id, 'BlacklistedChannels') or []
+    user_blacklist    = db.get_settings(message.server.id, 'BlacklistedUsers') or []
+
+    black_user      = message.author.id in user_blacklist
+    black_channel   = message.channel.id in channel_blacklist
+    server_is_black = db.get_settings(message.server.id, 'IsBlacklisted')
+
+    return (black_channel or black_user or server_is_black)
