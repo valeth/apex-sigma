4a9fc3b8d6871d382fe1f424e45d22536154d163
Patrick Auernig <patrick.auernig@gmail.com>
2017-02-15 06:21:56 +0100

    simplify blacklist.py
    remove some redundant checks and short-circuit if certain criteria
    aren't met

--- sigma/core/blacklist.py
+++ sigma/core/blacklist.py
@@ -2,23 +2,21 @@ from config import permitted_id
 
 
 def check_black(db, message):
-    black_channel = False
-    black_user = False
+    if message.author.id in permitted_id:
+        return False
+
+    if not message.server:
+        return False
+
+    black_channel   = False
+    black_user      = False
     server_is_black = False
-    black = False
-    if message.server:
-        channel_blacklist = db.get_settings(message.server.id, 'BlacklistedChannels')
-        if not channel_blacklist:
-            channel_blacklist = []
-        user_blacklist = db.get_settings(message.server.id, 'BlacklistedUsers')
-        if not user_blacklist:
-            user_blacklist = []
-        if message.author.id in user_blacklist:
-            black_user = True
-        if message.channel.id in channel_blacklist:
-            black_channel = True
-        server_is_black = db.get_settings(message.server.id, 'IsBlacklisted')
-    if message.author.id not in permitted_id:
-        if black_channel or black_user or server_is_black:
-            black = True
-    return black
+
+    channel_blacklist = db.get_settings(message.server.id, 'BlacklistedChannels') or []
+    user_blacklist    = db.get_settings(message.server.id, 'BlacklistedUsers') or []
+
+    black_user      = message.author.id in user_blacklist
+    black_channel   = message.channel.id in channel_blacklist
+    server_is_black = db.get_settings(message.server.id, 'IsBlacklisted')
+
+    return (black_channel or black_user or server_is_black)
